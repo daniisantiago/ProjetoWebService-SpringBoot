0b6021436c754a8a0911069f2e9d43e9e442d2ff
Daniella Santiago <danii.santiago@gmail.com>
2020-05-12 23:21:32 -0300

    implementação da camada de serviço e registro de componentes

---
+++ src/main/java/com/dani/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.dani.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dani.course.entities.User;
+import com.dani.course.repositories.UserRepository;
+
+@Service //registrando como um componente do Spring
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
