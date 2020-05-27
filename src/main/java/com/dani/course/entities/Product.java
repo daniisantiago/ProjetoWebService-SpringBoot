90a1b3e506333a35bccbbb897459bf89b0fe767b
Daniella Santiago <danii.santiago@gmail.com>
2020-05-27 11:48:17 -0300

    associaçao many to many entre categoria e produto

--- src/main/java/com/dani/course/entities/Product.java
+++ src/main/java/com/dani/course/entities/Product.java
@@ -8,23 +8,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;	
-	private String description;	
-	private Double price;	
+	private String name;
+	private String description;
+	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany @Fetch(FetchMode.JOIN)
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>(); //o set representa um conjunto... o mesmo produto n pode ter mais de uma categoria de uma vez
 	
 	public Product() {
