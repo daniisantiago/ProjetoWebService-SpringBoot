b1e1dc7e6b00924712577b11cd4a50e7396004bd
Daniella Santiago <danii.santiago@gmail.com>
2020-05-27 09:25:06 -0300

    criação da entidade Category

--- src/main/java/com/dani/course/config/TestConfig.java
+++ src/main/java/com/dani/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.dani.course.entities.Category;
 import com.dani.course.entities.Order;
 import com.dani.course.entities.User;
 import com.dani.course.entities.enums.OrderStatus;
+import com.dani.course.repositories.CategoryRepository;
 import com.dani.course.repositories.OrderRepository;
 import com.dani.course.repositories.UserRepository;
 
@@ -28,9 +30,16 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -42,6 +51,7 @@ public class TestConfig implements CommandLineRunner{
 		//salvando os dados no banco de dados
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 	
 	
