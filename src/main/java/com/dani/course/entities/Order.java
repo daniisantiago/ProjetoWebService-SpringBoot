3a9ad55a4c887128b47b10975ba107667442dc79
Daniella Santiago <danii.santiago@gmail.com>
2020-05-27 15:38:54 -0300

    criação dos metodos subTotal e total

--- src/main/java/com/dani/course/entities/Order.java
+++ src/main/java/com/dani/course/entities/Order.java
@@ -44,6 +44,7 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@Fetch(FetchMode.JOIN)
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
@@ -102,6 +103,14 @@ public class Order implements Serializable{
 
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 	
 	@Override
